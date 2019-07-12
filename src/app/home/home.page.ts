7fbdeaea4f8cd961f13929bc246a8ca8aac95269
Pravat Kiran <pravatkiran@mahuriventures.com>
2019-07-12 18:12:28 +0545

    added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { Chart } from 'chart.js';
-import { NavController, IonInfiniteScroll } from '@ionic/angular';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -14,9 +14,9 @@ export class HomePage {
 
   @ViewChild('doughnutCanvas') doughnutCanvas;
   @ViewChild('anotherCanvas') anotherCanvas;
-  // @ViewChild('lineCanvas') lineCanvas;
+  @ViewChild('lineCanvas') lineCanvas;
   doughnutChart: any;
-  // lineChart: any;
+  lineChart: any;
 
   constructor(
     public navCtrl: NavController
@@ -80,6 +80,36 @@ export class HomePage {
             "#FFCE56"
           ]
         }]
+      }
+    });
+    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+      type: "line",
+      data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [
+          {
+            label:'Daily Statement',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: "butt",
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: "miter",
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40],
+            spanGaps: true
+          }
+        ]
       }
     });
   } 
