e761ee3a2573477e381e6abb31c14babfebc5676
Pravat Kiran <pravatkiran@mahuriventures.com>
2019-07-16 16:06:37 +0545

    try to build

--- www/main.js
+++ www/main.js
@@ -441,6 +441,11 @@ var map = {
 		"./src/app/modules/category/category.module.ts",
 		"common",
 		"modules-category-category-module"
+	],
+	"./modules/product/product.module": [
+		"./src/app/modules/product/product.module.ts",
+		"common",
+		"modules-product-product-module"
 	]
 };
 function webpackAsyncContext(req) {
@@ -484,7 +489,8 @@ __webpack_require__.r(__webpack_exports__);
 var routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-    { path: 'category', loadChildren: './modules/category/category.module#CategoryModule' }
+    { path: 'category', loadChildren: './modules/category/category.module#CategoryModule' },
+    { path: 'product', loadChildren: './modules/product/product.module#ProductModule' }
 ];
 var AppRoutingModule = /** @class */ (function () {
     function AppRoutingModule() {
@@ -511,7 +517,7 @@ var AppRoutingModule = /** @class */ (function () {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<ion-app>\n  <ion-menu side=\"start\" contentId=\"first\">\n    <ion-header>\n      <ion-toolbar>\n        <ion-title>Menu</ion-title>\n      </ion-toolbar>\n    </ion-header>\n    <ion-content>\n      <ion-list>\n        <ion-item routerLink=\"/category\">Category</ion-item>\n        <ion-item>Product</ion-item>\n        <ion-item>Raw Material</ion-item>\n        <ion-item>Supplier</ion-item>\n        <ion-item>Purchase</ion-item>\n        <ion-item>Sales</ion-item>\n        <ion-item>Order</ion-item>\n        <ion-item>Inventory Items</ion-item>\n        <ion-item>Production</ion-item>\n        <ion-item>Reports</ion-item>\n        <ion-item>Setting</ion-item>\n      </ion-list>\n    </ion-content>\n  </ion-menu>\n  <ion-router-outlet id=\"first\"></ion-router-outlet>\n</ion-app>"
+module.exports = "<ion-app>\n  <ion-menu side=\"start\" contentId=\"first\">\n    <ion-header>\n      <ion-toolbar>\n        <ion-title>Menu</ion-title>\n      </ion-toolbar>\n    </ion-header>\n    <ion-content>\n      <ion-list>\n        <ion-item routerLink=\"/category\">Category</ion-item>\n        <ion-item routerLink=\"/product\">Product</ion-item>\n        <ion-item>Raw Material</ion-item>\n        <ion-item>Supplier</ion-item>\n        <ion-item>Purchase</ion-item>\n        <ion-item>Sales</ion-item>\n        <ion-item>Order</ion-item>\n        <ion-item>Inventory Items</ion-item>\n        <ion-item>Production</ion-item>\n        <ion-item>Reports</ion-item>\n        <ion-item>Setting</ion-item>\n      </ion-list>\n    </ion-content>\n  </ion-menu>\n  <ion-router-outlet id=\"first\"></ion-router-outlet>\n</ion-app>"
 
 /***/ }),
 
--- www/modules-category-category-module.js
+++ www/modules-category-category-module.js
@@ -1,5 +1,60 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([["modules-category-category-module"],{
 
+/***/ "./src/app/modules/category/category-form/category-form.component.html":
+/*!*****************************************************************************!*\
+  !*** ./src/app/modules/category/category-form/category-form.component.html ***!
+  \*****************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<ion-header>\n  <ion-toolbar>\n    <ion-buttons slot=\"start\">\n      <ion-back-button></ion-back-button>\n    </ion-buttons>\n    <ion-title>Hotel Inventory</ion-title>\n  </ion-toolbar>\n</ion-header>\n<ion-content>\n  <ion-card>\n    <ion-card-header>\n      <ion-card-title>Add Category</ion-card-title>\n    </ion-card-header>\n    <ion-card-content>\n      <ion-item>\n        <ion-label>Category Title</ion-label>\n        <ion-input placeholder=\"Category title\"></ion-input>\n      </ion-item>\n      <ion-item>\n        <ion-button slot=\"end\" type=\"button\" color=\"primary\">Save</ion-button>\n      </ion-item>\n    </ion-card-content>\n  </ion-card>\n</ion-content>"
+
+/***/ }),
+
+/***/ "./src/app/modules/category/category-form/category-form.component.scss":
+/*!*****************************************************************************!*\
+  !*** ./src/app/modules/category/category-form/category-form.component.scss ***!
+  \*****************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL21vZHVsZXMvY2F0ZWdvcnkvY2F0ZWdvcnktZm9ybS9jYXRlZ29yeS1mb3JtLmNvbXBvbmVudC5zY3NzIn0= */"
+
+/***/ }),
+
+/***/ "./src/app/modules/category/category-form/category-form.component.ts":
+/*!***************************************************************************!*\
+  !*** ./src/app/modules/category/category-form/category-form.component.ts ***!
+  \***************************************************************************/
+/*! exports provided: CategoryFormComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CategoryFormComponent", function() { return CategoryFormComponent; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+var CategoryFormComponent = /** @class */ (function () {
+    function CategoryFormComponent() {
+    }
+    CategoryFormComponent.prototype.ngOnInit = function () { };
+    CategoryFormComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-category-form',
+            template: __webpack_require__(/*! ./category-form.component.html */ "./src/app/modules/category/category-form/category-form.component.html"),
+            styles: [__webpack_require__(/*! ./category-form.component.scss */ "./src/app/modules/category/category-form/category-form.component.scss")]
+        }),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [])
+    ], CategoryFormComponent);
+    return CategoryFormComponent;
+}());
+
+
+
+/***/ }),
+
 /***/ "./src/app/modules/category/category-list/category-list.component.html":
 /*!*****************************************************************************!*\
   !*** ./src/app/modules/category/category-list/category-list.component.html ***!
@@ -7,7 +62,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<app-header></app-header>\n<ion-content>\n  <ion-card color=\"primary\">\n    <ion-card-header>\n      <ion-card-title>Categories</ion-card-title>\n    </ion-card-header>\n    <ion-list (click)='presentActionSheet()'>\n      <ion-item >\n        <ion-label>1</ion-label>\n        <ion-label>Cold Drink</ion-label>\n      </ion-item>\n      <ion-item>\n        <ion-label>2</ion-label>\n        <ion-label>Beer</ion-label>\n      </ion-item>\n      <ion-item>\n        <ion-label>3</ion-label>\n        <ion-label>Hard Drink</ion-label>\n      </ion-item>\n    </ion-list>\n  </ion-card>\n</ion-content>"
+module.exports = "<app-header></app-header>\n<ion-content>\n  <ion-card color=\"primary\">\n    <ion-card-header>\n      <ion-card-title>Categories</ion-card-title>\n    </ion-card-header>\n    <ion-list (click)='presentActionSheet()'>\n      <ion-item>\n        <ion-label>1</ion-label>\n        <ion-label>Cold Drink</ion-label>\n      </ion-item>\n      <ion-item>\n        <ion-label>2</ion-label>\n        <ion-label>Beer</ion-label>\n      </ion-item>\n      <ion-item>\n        <ion-label>3</ion-label>\n        <ion-label>Hard Drink</ion-label>\n      </ion-item>\n    </ion-list>\n  </ion-card>\n  <ion-fab vertical=\"bottom\" horizontal=\"end\" slot=\"fixed\">\n    <ion-fab-button routerLink=\"add\">\n      <ion-icon name=\"add\"></ion-icon>\n    </ion-fab-button>\n  </ion-fab>\n</ion-content>"
 
 /***/ }),
 
@@ -114,12 +169,15 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
 /* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
 /* harmony import */ var _category_list_category_list_component__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./category-list/category-list.component */ "./src/app/modules/category/category-list/category-list.component.ts");
+/* harmony import */ var _category_form_category_form_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./category-form/category-form.component */ "./src/app/modules/category/category-form/category-form.component.ts");
+
 
 
 
 
 var categoryRoutes = [
-    { path: '', component: _category_list_category_list_component__WEBPACK_IMPORTED_MODULE_3__["CategoryListComponent"] }
+    { path: '', component: _category_list_category_list_component__WEBPACK_IMPORTED_MODULE_3__["CategoryListComponent"] },
+    { path: 'add', component: _category_form_category_form_component__WEBPACK_IMPORTED_MODULE_4__["CategoryFormComponent"] }
 ];
 var CategoryRoutingModule = /** @class */ (function () {
     function CategoryRoutingModule() {
@@ -158,6 +216,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _category_routing_module__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./category-routing.module */ "./src/app/modules/category/category-routing.module.ts");
 /* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/dist/fesm5.js");
 /* harmony import */ var src_app_shared_shared_module__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! src/app/shared/shared.module */ "./src/app/shared/shared.module.ts");
+/* harmony import */ var _category_form_category_form_component__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./category-form/category-form.component */ "./src/app/modules/category/category-form/category-form.component.ts");
+
 
 
 
@@ -171,13 +231,14 @@ var CategoryModule = /** @class */ (function () {
     CategoryModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
             declarations: [
-                _category_list_category_list_component__WEBPACK_IMPORTED_MODULE_3__["CategoryListComponent"]
+                _category_list_category_list_component__WEBPACK_IMPORTED_MODULE_3__["CategoryListComponent"],
+                _category_form_category_form_component__WEBPACK_IMPORTED_MODULE_7__["CategoryFormComponent"]
             ],
             imports: [
                 _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
+                _category_routing_module__WEBPACK_IMPORTED_MODULE_4__["CategoryRoutingModule"],
                 _ionic_angular__WEBPACK_IMPORTED_MODULE_5__["IonicModule"],
                 src_app_shared_shared_module__WEBPACK_IMPORTED_MODULE_6__["SharedModule"],
-                _category_routing_module__WEBPACK_IMPORTED_MODULE_4__["CategoryRoutingModule"],
             ]
         })
     ], CategoryModule);
