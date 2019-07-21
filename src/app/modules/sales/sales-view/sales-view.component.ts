13e08ce9320f47ca8399d2c3148d9290b66a634e
Pravat Kiran <pravatkiran@mahuriventures.com>
2019-07-21 14:26:54 +0545

    sales view added

---
+++ src/app/modules/sales/sales-view/sales-view.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-view',
+  templateUrl: './sales-view.component.html',
+  styleUrls: ['./sales-view.component.scss'],
+})
+export class SalesViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
